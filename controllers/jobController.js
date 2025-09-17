d62c536144130dacbcd3e89b9b8779b7dc4cd50e
CristhianHatzman <cristhianhatzman@gmail.com>
2025-09-17 12:59:07 -0300

    feat: adiciona controller e router de jobs da api

---
+++ controllers/jobController.js
@@ -0,0 +1,105 @@
+import { ObjectId } from "mongodb";
+import JobService from "../services/JobService.js";
+
+const getAllJobs = async (req, res) => {
+  try {
+    const jobs = await JobService.GetAll();
+    res.status(200).json({ jobs: jobs });
+    // Cód. 200: OK - A solicitação foi bem-sucedida.
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Erro interno no servidor." });
+    // Cód. 500: Erro Interno do Servidor - Ocorreu um erro no servidor ao processar a solicitação.
+  }
+};
+
+const createJob = async (req, res) => {
+  try {
+    const {
+      title,
+      seniority,
+      company,
+      type,
+      location,
+      salary,
+      description,
+      jobFunction,
+      requirements,
+    } = req.body;
+    await JobService.CreateJob(
+      title,
+      seniority,
+      company,
+      type,
+      location,
+      salary,
+      description,
+      jobFunction,
+      requirements
+    );
+    res.status(201).send({ message: "Job criado com Sucesso!" });
+    // Cód. 201: Created - A solicitação foi bem-sucedida e um novo recurso foi criado como resultado.
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Erro interno no servidor." });
+  }
+};
+
+const deleteJob = async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params;
+      const deletedJob = await JobService.DeleteJob(id);
+      if (deletedJob) {
+        res.status(200).send({ message: "Job deletado com sucesso!" });
+      } else {
+        res.status(404).send({ error: "Job não encontrado." });
+        // Cód. 404: Not Found - O recurso solicitado não foi encontrado.
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Erro interno no servidor." });
+  }
+};
+
+const UpdateJob = async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params;
+      const {
+        title,
+        seniority,
+        company,
+        type,
+        location,
+        salary,
+        description,
+        jobFunction,
+        requirements,
+      } = req.body;
+      const updatedJob = await JobService.UpdateJob(
+        id,
+        title,
+        seniority,
+        company,
+        type,
+        location,
+        salary,
+        description,
+        jobFunction,
+        requirements
+      );
+      if (updatedJob) {
+        res.status(200).send({ message: "Job atualizado com sucesso!" });
+      } else {
+        res.status(404).send({ error: "Job não encontrado." });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Erro interno no servidor." });
+  }
+};
+
+export default { getAllJobs, createJob, deleteJob, UpdateJob };
